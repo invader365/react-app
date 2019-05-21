4d255c88adefcd5df7c9f25ea33c795779136568
abelricaurte <abel.x.ricaurte.-nd@disney.com>
2019-05-21 14:18:58 -0500

    First commit

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Components from "./components/Components";
-
-ReactDOM.render(<Components />, document.getElementById("root"));
