cdadce2adeb856e554b7174a4a0af32d3115679f
abelricaurte <abel.x.ricaurte.-nd@disney.com>
2019-05-21 15:14:03 -0500

    Class based components practice

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+import Header from "./Header";
+import Greeting from "./Greeting";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header username="Abel" />
+        <Greeting />
+      </div>
+    );
+  }
+}
+
+export default App;
