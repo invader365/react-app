cc3b24bb569086f1f42aa9062f4af78eee959bc1
abelricaurte <abel.x.ricaurte.-nd@disney.com>
2019-05-21 14:10:50 -0500

    including class components

--- src/components/Components.js
+++ src/components/Components.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Components() {
-  return (
-    <div>
-      <br />
-    </div>
-  );
+// function Components() {
+//   return (
+//     <div>
+//       <br />
+//     </div>
+//   );
+// }
+
+class Components extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hello World!</p>
+      </div>
+    );
+  }
 }
 
 export default Components;
