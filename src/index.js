d24a13595a9d9c0e64831f82c6f1b628550afe8d
abelricaurte <abel.x.ricaurte.-nd@disney.com>
2019-05-21 10:40:07 -0500

    develop initial files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import App from "./components/App";
+import Components from "./components/Components";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Components />, document.getElementById("root"));
