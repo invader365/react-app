d37f2693e9be609ae4e2c35f8d37b54366bf843a
abelricaurte <abel.x.ricaurte.-nd@disney.com>
2019-06-27 16:20:54 -0500

    Conditional rendering practice

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React {Component, Fragment} from "react"
+
+class Main extends Component {
+    render() {
+        return (
+          <Fragment>
+            <h1>Hey ya</h1>
+            <p>Lorem Ipsum Dolor Set</p>
+          </Fragment>
+        )
+    }
+
+}
+
+export default Main
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Components from "./components/Components";
-
-ReactDOM.render(<Components />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
